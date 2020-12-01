c75caa12dbf8aa79fbf3249097c9556d66d7314a
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-01 22:04:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
   
-  def initialize
-    hash  = [:name, :birthday, :hair_color, :eye_color, :height, 
+  def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width]
+:wrist_size, :glove_size, :pant_length, :pant_width)
+    
   end
   
 end
